4637235802dac52fff8fa52eb3d329099011c876
imadovetch <ibouderoua63@gmail.com>
2023-09-21 21:48:42 +0100

    trying some brain.f

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
+#include "monty.h"
 
 int push_argument = 0;
 
--- somefunc.c
+++ somefunc.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 /**
  * pchar - Print the character at the top of the stack.
